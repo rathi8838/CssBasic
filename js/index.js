abd1b42dacd457364a7799f78f7356832c728d49
rathi8838 <rathisankardass@gmail.com>
2025-03-19 12:16:05 +0530

    master

---
+++ js/index.js
@@ -0,0 +1,262 @@
+// console.log("hello Mousiya...");
+
+
+// prompt('Enter Your Name');
+// alert('warning,,,,');
+// confirm('r u exit')
+
+
+// console.log(prompt(prompt("enter your name")));
+// alert(prompt("enter your name")); 
+
+
+
+// var UserName = "Suryrakj";
+
+// let Name = 1234567890;
+
+// Name= "surya";
+
+
+// const Num = 12;
+
+// Num =13;
+
+
+
+
+// console.log(UserName);
+
+
+// print()
+
+// close();
+
+// open('https://infomedia.com/');
+
+
+
+// let a = 123;
+
+// console.log(a.toString());
+
+// let b = "10";
+
+// console.log(parseInt(b));
+
+
+
+
+
+
+// let Username="Suryaraj";
+
+
+// console.log(Username);
+// console.log(Username.length);
+// console.log(Username[0]);
+// console.log(Username.concat(Username))
+// console.log(Username.lastIndexOf("r"));
+// console.log(Username.repeat(5));
+// console.log(Username.slice(0,5));
+// console.log(Username.toUpperCase());
+
+
+// Username[9]="idm"
+
+// console.log(Username);
+
+
+
+// console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.abs(80));
+// console.log(Math.ceil(8.1));
+// console.log(Math.floor(12.333333333333333));
+// console.log(Math.max(80,90));
+// console.log(Math.min(70,90));
+// console.log(Math.pow(2,5));
+// console.log(Math.random());
+// console.log(Math.round(4.5));
+
+
+//  + - * / %
+//  = != 
+
+// < > <= >=  != ==  ====;
+
+//   AND &&   OR ||   NOT ! 
+ 
+// ++  --
+
+// NOR XOR 
+
+//  value ? true : false;
+
+ 
+// if(10<4){
+//     console.log("Value is crt..");   
+// }
+
+// else if((10<40)&&(40<6)){
+//     console.log("2nd crt...");
+
+// }
+
+// else if((10<50)||(10<5)){
+    // console.log("3rd crt..");
+    
+
+// }
+
+// else{
+//     console.log("Wrong...");
+    
+// }
+// 
+    // if(20<40)
+    // {
+        // console.log("2nd crt..");
+        // if(30<5){
+            // console.log("3rd crt..");
+        // }
+    // }
+// }
+
+
+//
+            
+//         }
+//     }
+    
+// }
+
+
+
+// let a = [1,2,3,4.5,'s',"Suryraj"];
+
+// console.log(a);
+// console.log(a[0]);
+// console.log(a.concat(a));
+// console.log(a.length);
+
+// a[6]="welcome";
+// console.log(a);
+
+// a.push(90)
+// a.push(91)
+// a.push(92)
+
+// a.pop();
+
+
+// a.unshift(80)
+// a.unshift(81)
+// a.unshift(82)
+
+// a.shift()
+
+
+//a.splice(3,0,"KIA");
+
+
+// console.log(a);
+
+// let arr = [
+//     [1,2,3,4,5,6,7],[1,2,,3,4,5,['s','u','r','i','y','a']]
+
+// ]
+
+
+
+// const Arr = [1,2,3,4];
+
+// Arr.push(90);
+// console.log(Arr);
+
+
+
+// let obj ={
+//     Name:"SURYRAJ",
+//     age:12,
+//     city:"Erode",
+//     brother:{
+//         Name:"Karthick"
+//     }
+// }
+
+// console.log(obj.brother.Name);
+
+
+let ArrObj =[
+    {Name:"SURYRAJ1",Age: 12},
+    {Name:"SURYRAJ2",Age:13},
+    {Name:"SURYRAJ3",Age:14},
+    {Name:"SURYRAJ4",Age:15},
+    {Name:"SURYRAJ5",Age:16},
+    {Name:"SURYRAJ6",Age:17},
+    {Name:"SURYRAJ7",Age:18},
+    {Name:"SURYRAJ8",Age :19},]
+
+
+    
+// }
+
+    // for(let i=0;i<9;i++){   //i=i+1
+
+    //     console.log(i);
+        
+    // }
+
+//     let a=10
+//     while(a<20){
+//         if(a===15){
+//             break;
+//         }
+//         console.log(a);
+//         a++
+
+//     }
+// let b =20;
+
+//     do{
+//         console.log(b);
+//         b++
+        
+//     }while(b<30)
+
+
+
+// for(let i=0;i<ArrObj.length;i++){
+//     console.log(ArrObj[i].Name);
+    
+// }
+
+// for( let b in ArrObj){
+//     console.log(b);
+    
+// }
+
+// ArrObj.forEach((y)=>{
+//     console.log(y);
+    
+// })
+
+// ArrObj.map((t)=>{
+//     console.log(t.Name);
+    
+// })
+
+
+      
+
+
+
+
+ 
+
+
+
+
+
+
